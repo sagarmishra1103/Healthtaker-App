92741cfb4c7fbe3cd75367827cfe45afba958048
shivsagar mishra <shivsagarm50@gmail.com>
2025-02-27 13:01:45 +0530

    Frontend Filess Added

--- db.py
+++
@@ -1,9 +0,0 @@
-from flask_pymongo import PyMongo
-from flask import Flask
-
-app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb+srv://sagar:mishra@cluster0.v70j0.mongodb.net/helthtaker'
-mongo = PyMongo(app)
-
-
-
